fb26936ec45ad65aa457cde52d1b6f0e55fb8152
jaywmdenesik <32952992+jaywmdenesik@users.noreply.github.com>
2017-10-27 10:49:25 -0600

    Add files via upload

--- Order.java
+++ Order.java
@@ -12,13 +12,18 @@ public class Order
 	
 	public Order(){};
 	public Date getDateRecieved(){return dateRecieved;};
-	public static Double calculatePrice(Product[] product)
+	public static Double calculatePrice(Product[] product)   //*****Calculate Price*****
 	{
 		double basePrice = 0;
 		
-		for (int i = 0; i < 10; i++)
-		{basePrice += product[i].getPrice();}
+		for (int i = 0; i < 10; i++) //for all 10 products
+		{
+			basePrice += product[i].getPrice();		// add current products price to total price
+			System.out.println("'" + product[i].getName() + "' costs $" + product[i].getPrice() + ""); // inform user
+			System.out.println("Your total cost is now $" + basePrice + "");		
+		}
 		Customer customer = new Customer("","",0,0.9);
+		System.out.println("Your total price before your 10% discount is $" + basePrice + ""); // print total 'pre-discount' price
 		price = basePrice * customer.getDiscountRating();
 		
 		return price;
