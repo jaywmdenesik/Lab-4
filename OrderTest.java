5212cd391c0481ff299097326746db7a9379177c
jaywmdenesik <32952992+jaywmdenesik@users.noreply.github.com>
2017-10-27 07:49:57 -0600

    Add files via upload

--- OrderTest.java
+++ OrderTest.java
@@ -8,5 +8,25 @@ public class OrderTest
 {
 	public static void main(String[] args)
 	{
+		Scanner in = new Scanner(System.in);
+		Product[] product;
+		Product holder;
+		product = new Product[10];
+		try
+		{
+		Scanner inFile = new Scanner(new FileReader("ProductCatalogue.txt"));
+		String test = "";
+		double test2 = 0;
+		int test3 = 0;
+		String fixer = "";
+		for (int i = 0; i < 10; i++)
+		{
+	    product[i] = new Product(inFile.nextLine(),inFile.nextDouble(),inFile.nextInt());
+		fixer = inFile.nextLine();
+		}
+	
+		}
+		catch (IOException e)
+		{e.printStackTrace();}
 	}
 }
