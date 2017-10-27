bd9ce9d6024a2dc8b016d36b0850e3e8486cc246
jaywmdenesik <32952992+jaywmdenesik@users.noreply.github.com>
2017-10-27 08:31:56 -0600

    Add files via upload

--- OrderApplication.java
+++ OrderApplication.java
@@ -7,6 +7,39 @@ import java.util.Scanner;
 public class OrderApplication
 {
 	
-	private boolean importCatalogue(){return false;};
-	public boolean createOrder(Order order){return true;};
+	
+		private static boolean importCatalogue()
+		{
+			Scanner in = new Scanner(System.in);												
+			Product[] product;
+			Product holder;
+			product = new Product[10];
+		
+			try
+			{
+			  Scanner inFile = new Scanner(new FileReader("ProductCatalogue.txt"));
+			  String fixer = "";
+			
+			for (int i = 0; i < 10; i++)
+				{
+			      product[i] = new Product(inFile.nextLine(),inFile.nextDouble(),inFile.nextInt());
+				  fixer = inFile.nextLine();
+				}
+			}
+		
+			catch (IOException e)
+			{e.printStackTrace();}
+			return true;
+		};
+		
+		public boolean createOrder(Order order)
+		{
+		this.importCatalogue();
+		return true;
+		};
+		
+		public static void main(String[] args)
+	{
+		importCatalogue();
+	}
 };
